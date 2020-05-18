cbfd4faf12c42e498e952fb293e7ff71a661af0c
kaban0007 <kaban0007@gmail.com>
2020-05-18 17:32:16 +0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,4 @@ export class AppComponent  {}
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at http://angular.io/license
-*/
+*/ 
